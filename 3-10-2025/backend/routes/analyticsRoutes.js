65981c6dddfc69a1eadbd4f25dda169f889078ed
Sujit Kumar <5215ssingh@gmail.com>
2025-10-03 17:25:41 +0530

    updated

--- 3-10-2025/backend/routes/analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getLeads, createLead, updateLead, deleteLead } = require("../controllers/leadController");
-const protect = require("../middleware/authMiddleware");
-
-router.route("/").get(protect, getLeads).post(protect, createLead);
-router.route("/:id").put(protect, updateLead).delete(protect, deleteLead);
-
-module.exports = router;
